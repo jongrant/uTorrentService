78d58144c039f738c2486fbe5c49c8914bc54130
jongrant <jongrant@gmail.com>
2013-07-28 17:00:53 +0100

    Initial version.

---
+++ uTorrentService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uTorrentService
+{
+    public partial class uTorrentService : ServiceBase
+    {
+        private Process process;
+
+        public uTorrentService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string exeFile = Environment.ExpandEnvironmentVariables("%AppData%\\uTorrent\\uTorrent.exe");
+
+            ProcessStartInfo psi = new ProcessStartInfo(exeFile);
+            psi.UseShellExecute = true;
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+
+            process = Process.Start(psi);
+            process.WaitForInputIdle();
+        }
+
+        protected override void OnStop()
+        {
+            process.Close();
+            process.Dispose();
+        }
+    }
+}
