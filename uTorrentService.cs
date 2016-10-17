24762d02f2f0667353b5733f3565bec564591578
Lukas Holota <l.holota@outlook.com>
2016-10-17 12:06:47 +0200

    Added support for a switch to specify custom path to the utorrent exe file and detection when the exe is in the same directory. Added error logging when the file is not found or an error occurs during the execution.

--- uTorrentService.cs
+++ uTorrentService.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace uTorrentService
 {
+    using System.IO;
+
     public partial class uTorrentService : ServiceBase
     {
         private Process process;
@@ -21,20 +17,60 @@ namespace uTorrentService
 
         protected override void OnStart(string[] args)
         {
-            string exeFile = Environment.ExpandEnvironmentVariables("%AppData%\\uTorrent\\uTorrent.exe");
+            var exePath = GetExeLocation(args);
 
-            ProcessStartInfo psi = new ProcessStartInfo(exeFile);
+            if (!File.Exists(exePath))
+            {
+                var message = string.Format("The file {0} could not be found.", exePath);
+                this.EventLog.WriteEntry(message, EventLogEntryType.Error);
+
+                Environment.Exit(1);
+            }
+
+            ProcessStartInfo psi = new ProcessStartInfo(exePath);
             psi.UseShellExecute = true;
+            psi.CreateNoWindow = true;
             psi.WindowStyle = ProcessWindowStyle.Hidden;
 
-            process = Process.Start(psi);
-            process.WaitForInputIdle();
+            try
+            {
+                this.process = Process.Start(psi);
+
+                if (this.process != null)
+                {
+                    process.WaitForInputIdle();
+                }
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format("Executing the utorrent.exe failed: {0}", ex);
+                this.EventLog.WriteEntry(message, EventLogEntryType.Error);
+
+                Environment.Exit(2);
+            }
         }
 
         protected override void OnStop()
         {
             process.Close();
             process.Dispose();
+        }
+
+        private string GetExeLocation(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                return args[0];
+            }
+
+            var sameDirPath = Path.Combine(Environment.CurrentDirectory, "uTorrent.exe");
+
+            if (File.Exists(sameDirPath))
+            {
+                return sameDirPath;
+            }
+
+            return Environment.ExpandEnvironmentVariables("%AppData%\\uTorrent\\uTorrent.exe");
         }
     }
 }
